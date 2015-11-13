2b457008645635d26646ccde2b37ffcbdb003d27
Felix Li <lcfelixli@ust.hk>
2015-11-13 21:31:11 +0800

    feat: Added JSON for bio and education

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,73 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+	"name" : "Felix Li",
+	"role" : "Front-end Developer",
+	"contacts" : {
+		"mobile" : "+852 1234-5678",
+		"email" : "liyb.gz@gmail.com",
+		"github" : "https://github.com/liyb-gz",
+		"twitter" : "LiybGz",
+		"location" : "Hong Kong"
+	},
+	"welcomeMessage": "Donec vel diam tempor mi feugiat bibendum nec id odio. Ut nec bibendum lorem. Etiam ultrices, risus nec blandit finibus, nisl purus fringilla metus, sit amet elementum nibh leo ac lorem. Morbi volutpat, diam sed porta iaculis, libero sapien feugiat lorem, eget consectetur dolor mauris consequat eros. Quisque bibendum, ante sed ultrices ultricies, eros ex elementum risus, id tincidunt libero purus sit amet quam. Nullam id ex eleifend, luctus lacus a, blandit nisl. Sed molestie, sapien at pharetra tincidunt, lectus augue dictum magna, non tempus ipsum sem id augue. Nulla luctus scelerisque mi, eu maximus metus eleifend ac.",
+	"skills" : [
+		"Graphic Design",
+		"HTML Coding",
+		"JavaScript Programming",
+		"Project Managing"
+	],
+	"biopic" : "http://lorempixel.com/400/400/people/"
+};
+
+bio.display = function () {
+
+};
+
+var education = {
+	"schools" : [
+		{
+			"name" : "ABC University",
+			"location" : "Guangzhou",
+			"degree" : "Bachelor of Engineering",
+			"majors" : [
+				"Eletrical Engineering",
+				"Computer Science"
+			],
+			"dates" : 20110701,
+			"url" : "http://www.abcuniversity.edu.cn"
+		},
+		{
+			"name" : "XYZ University",
+			"location" : "Hong Kong",
+			"degree" : "Master of Science",
+			"majors" : [
+				"Information Technology"
+			],
+			"dates" : 20120701,
+			"url" : "http://www.xyzu.hk"
+		}
+	],
+	"onlineCourses" : [
+		{
+			"title" : "Coursera Specialization: Learn to Program and Analyze Data with Python",
+			"school" : "University of Michigan",
+			"date": 20130124,
+			"url" : "https://www.coursera.org/specializations/python"
+		},
+		{
+			"title" : "Coursera Specialization: Introduction to Project Management Principles and Practices Specialization",
+			"school" : "University of California, Irvine",
+			"date": 20140726,
+			"url" : "https://www.coursera.org/specializations/project-management"
+		},
+		{
+			"title" : "Coursera Specialization: Interaction Design Specialization",
+			"school" : "University of California, San Diego",
+			"date": 20150316,
+			"url" : "https://www.coursera.org/specializations/interaction-design"
+		}
+	]
+};
+
+education.display = function () {
+
+};
